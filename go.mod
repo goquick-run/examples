3a24b21cc2e720bb6bfafad9c83387ca54755be9
jeffotoni <jeff.otoni@gmail.com>
2025-03-03 16:53:17 -0300

    new version quick

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/goquickrun/exemplos
 
 go 1.24.0
 
-require github.com/jeffotoni/quick v0.0.0-20250303062315-fce775ad4dfa
+require github.com/jeffotoni/quick v0.0.0-20250303154016-753fc050c1ba
--- quick.http.client/retry.post/retry.post.go
+++ quick.http.client/retry.post/retry.post.go
@@ -7,36 +7,38 @@ import (
 	"github.com/jeffotoni/quick/http/client"
 )
 
+// Retry Delay Format Support
+//
+// The retry delay parameter supports various formats for flexibility in defining the wait time between retries.
+// Additionally, it allows enabling exponential backoff by appending "-bex".
+//
+// Supported formats:
+//
+// - "2mil"      => 2 milliseconds
+// - "2s"        => 2 seconds
+// - "2min"      => 2 minutes
+// - "2mil-bex"  => 2 milliseconds with exponential backoff
+// - "2s-bex"    => 2 seconds with exponential backoff
+// - "2min-bex"  => 2 minutes with exponential backoff
+//
+// When using the "-bex" suffix, the delay will increase exponentially on each retry attempt
+// using the formula: waitTime = baseDelay * (2^attempt).
+//
+// Example Usage:
+//
+// client.WithRetry(
+//
+//		3,                 // Maximum number of retries
+//		"2s",              // Delay between attempts
+//		true,              // Use exponential backoff
+//		"500,502,503,504", // HTTP status for retry
+//		true,              // show Logger
+//	),
+//
+// This configuration will retry up to 3 times with an exponential backoff starting at 2 seconds,
+// and will only retry if the response status is 500, 502, 503, or 504.
 func main() {
-	// Retry Delay Format Support
-	//
-	// The retry delay parameter supports various formats for flexibility in defining the wait time between retries.
-	// Additionally, it allows enabling exponential backoff by appending "-bex".
-	//
-	// Supported formats:
-	//
-	// - "2mil"      => 2 milliseconds
-	// - "2s"        => 2 seconds
-	// - "2min"      => 2 minutes
-	// - "2mil-bex"  => 2 milliseconds with exponential backoff
-	// - "2s-bex"    => 2 seconds with exponential backoff
-	// - "2min-bex"  => 2 minutes with exponential backoff
-	//
-	// When using the "-bex" suffix, the delay will increase exponentially on each retry attempt
-	// using the formula: waitTime = baseDelay * (2^attempt).
-	//
-	// Example Usage:
-	//
-	// client.WithRetry(
-	// 		3,                 // Maximum number of retries
-	// 		"2s",              // Delay between attempts
-	// 		true,              // Use exponential backoff
-	// 		"500,502,503,504", // HTTP status for retry
-	// 		true,              // show Logger
-	// 	),
-	//
-	// This configuration will retry up to 3 times with an exponential backoff starting at 2 seconds,
-	// and will only retry if the response status is 500, 502, 503, or 504.
+
 	cClient := client.New(
 		client.WithRetry(
 			3,                 // Maximum number of retries
